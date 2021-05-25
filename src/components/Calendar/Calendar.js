2ec7f9068c94bf5a454da2870819d222b63ef430
Some-0ne-Else <cyberdef@gmail.com>
2021-05-25 23:03:02 +0300

    basic functionality for CalendarEvent component

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import CalendarDescription from '../CalendarDescription/CalendarDescription';
 import CalendarConfirmation from '../CalendarConfirmation/CalendarConfirmation';
 import CalendarSuccessRegistrationPopup from '../CalendarSuccessRegistrationPopup/CalendarSuccessRegistrationPopup';
+import CalendarEvent from '../CalendarEvent/CalendarEvent';
 import api from '../../utils/Api';
 // import { CalendarContext } from "../../contexts/CalendarContext";
 
@@ -14,20 +15,7 @@ function Calendar() {
   React.useEffect(() => {
     api.getCalendar()
       .then((res) => {
-        setCalendarData(
-          res.calendar.map((item) => ({
-            _id: item.id,
-            title: item.title,
-            address: item.address,
-            booked: item.booked,
-            city: item.city,
-            contact: item.contact,
-            description: item.description,
-            seats: item.seats,
-            takenSeats: item.takenSeats,
-            date: item.startAt,
-          })),
-        );
+        setCalendarData(res.calendar);
         // eslint-disable-next-line no-console
         console.log('calendarData', calendarData);
       })
@@ -64,53 +52,19 @@ function Calendar() {
       <main className="main">
         <section className="calendar-container page__section">
           {calendarData.map((calendar) => (
-            <article className="calendar calendar_selected">
-              <div className="calendar__caption">
-                <div className="calendar__info">
-                  <p className="calendar__type">Волонтёры + дети</p>
-                  <p className="calendar__weekday">Декабрь / понедельник</p>
-                </div>
-                <div className="calendar__about">
-                  <h2 className="section-title calendar__title">
-                    {calendar.title}
-                  </h2>
-                  <p className="calendar__date">05</p>
-                </div>
-              </div>
-              <div className="calendar__meetup">
-                <ul className="calendar__info-list">
-                  <li className="calendar__info-item">
-                    <p className="calendar__time">12:00–14:00</p>
-                  </li>
-                  <li className="calendar__info-item">
-                    <p className="calendar__place">
-                      {calendar.address}
-                    </p>
-                  </li>
-                  <li className="calendar__info-item">
-                    <p className="calendar__contact">{calendar.contact}</p>
-                    <p className="calendar__contact" />
-                  </li>
-                </ul>
-                <div className="calendar__submit">
-                  <button
-                    className="button button_theme_light calendar__button calendar__button_selected calendar__button_action_sign-up"
-                    type="button"
-                    onClick={handleCancelClick}
-                  >
-                    Отменить запись
-                  </button>
-                  <p className="calendar__place-left" />
-                  <button
-                    className="button calendar__button-dots button_theme_light"
-                    type="button"
-                    onClick={handleOnDescriptionClick}
-                  >
-                    &#8226;&#8226;&#8226;
-                  </button>
-                </div>
-              </div>
-            </article>
+            <CalendarEvent
+              key={calendar.id}
+              booked={calendar.booked}
+              title={calendar.title}
+              address={calendar.address}
+              contact={calendar.contact}
+              startAt={calendar.startAt}
+              endAt={calendar.endAt}
+              seats={calendar.seats}
+              takenSeats={calendar.takenSeats}
+              onCancel={handleCancelClick}
+              onDescription={handleOnDescriptionClick}
+            />
           ))}
         </section>
       </main>
