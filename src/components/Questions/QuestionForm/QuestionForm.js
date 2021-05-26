23f9061667b85de5720a1bd59a7dd8c0e51e011f
script-ninja <61618647+script-ninja@users.noreply.github.com>
2021-05-26 23:13:24 +0500

    feat: блокировка кнопки отправки

--- src/components/Questions/QuestionForm/QuestionForm.js
+++ src/components/Questions/QuestionForm/QuestionForm.js
@@ -3,12 +3,29 @@ import React from 'react';
 import './QuestionForm.scss';
 
 export default function QuestionForm() {
+  const [question, setQuestion] = React.useState('');
+
+  function onInput(event) {
+    document.forms['add-question'].submit.disabled = event.target.value.length < 1;
+    setQuestion(event.target.value);
+  }
+
+  function onSubmit(event) {
+    event.preventDefault();
+
+    const submitButton = event.target.submit;
+    submitButton.disabled = true;
+    event.target.reset();
+    // eslint-disable-next-line no-console
+    console.log(question);
+  }
+
   return (
     <section className="add-question page__section">
       <h2 className="section-title add-question__title">Если вы не нашли ответ на свой вопрос — напишите нам, и мы включим его в список</h2>
-      <form className="question-form" name="add-question">
+      <form onSubmit={onSubmit} className="question-form" name="add-question">
         <fieldset className="question-form__add-question">
-          <input className="question-form__input" type="text" name="question" value="" placeholder="Введите вопрос" required />
+          <input onInput={onInput} className="question-form__input" type="text" name="question" placeholder="Введите вопрос" required />
           <button className="button button_theme_light question-form__button" type="submit" name="submit" disabled>Отправить</button>
         </fieldset>
       </form>
