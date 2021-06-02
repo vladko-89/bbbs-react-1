dbbe47018b8a05fe44596937adb90cc9bc574083
Some-0ne-Else <cyberdef@gmail.com>
2021-06-02 19:18:17 +0300

    smooth scroll on footer nav

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,13 +1,10 @@
 const token = '';
 const baseUrl = 'http://localhost:3000';
 const delayResponse = 500;
+const scrollDelay = 100;
 const cardsPerPage = 1;
-const placesTextForms = [
-  'место',
-  'места',
-  'мест',
-];
+const placesTextForms = ['место', 'места', 'мест'];
 
 export {
-  baseUrl, token, delayResponse, cardsPerPage, placesTextForms,
+  baseUrl, token, delayResponse, scrollDelay, cardsPerPage, placesTextForms,
 };
