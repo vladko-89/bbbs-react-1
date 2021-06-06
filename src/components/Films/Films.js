e979ba251b2899ceb585c9de429ac419f20530dc
script-ninja <61618647+script-ninja@users.noreply.github.com>
2021-06-06 22:13:02 +0500

    feat: фильтрация фильмов по тегам

--- src/components/Films/Films.js
+++ src/components/Films/Films.js
@@ -1,18 +1,52 @@
 import React from 'react';
+import Tag from './Tag/Tag';
 import FilmCard from './FilmCard/FilmCard';
 import Pagination from '../Pagination/Pagination';
 
-import { films } from '../../utils/filmsData';
+import { films, filmTags } from '../../utils/filmsData';
 
 export default function Films() {
   const cardsPerPage = 16;
+  const [tagIdArray, setTagIdArray] = React.useState([]);
+  const [filteredFilms, setFilteredFilms] = React.useState(films);
   const [shownCards, setShownCards] = React.useState([]);
 
   function onPageChange(currPage) {
     const begin = currPage * cardsPerPage - cardsPerPage;
     const end = begin + cardsPerPage;
-    setShownCards(films.slice(begin, end < films.length ? end : films.length));
+    setShownCards(
+      filteredFilms.slice(begin, end < filteredFilms.length ? end : filteredFilms.length),
+    );
   }
+
+  function filterByTags(tags, data) {
+    return (!tags.length)
+      ? data
+      : data.filter(
+        (item) => item.tags.some((itemTag) => tags.some((tagId) => tagId === itemTag.id)),
+      );
+  }
+
+  function toggleTagId(tagId) {
+    if (tagId === 0) {
+      setTagIdArray([]);
+    } else {
+      const index = tagIdArray.findIndex((tag) => tag === tagId);
+      if (index >= 0) {
+        setTagIdArray(tagIdArray.filter((tag) => tag !== tagId));
+      } else {
+        setTagIdArray([...tagIdArray, tagId]);
+      }
+    }
+  }
+
+  React.useEffect(() => {
+    setFilteredFilms(filterByTags(tagIdArray, films));
+  }, [tagIdArray]);
+
+  React.useEffect(() => {
+    onPageChange(1);
+  }, [filteredFilms]);
 
   return (
     <main className="main">
@@ -20,33 +54,21 @@ export default function Films() {
         <h1 className="main-title">Фильмы</h1>
         <div className="tags">
           <ul className="tags__list">
-            <li className="tags__list-item">
-              <button className="button tags__button tags__button_active" type="button">Все</button>
-            </li>
-            <li className="tags__list-item">
-              <button className="button tags__button" type="button">Документальный</button>
-            </li>
-            <li className="tags__list-item">
-              <button className="button tags__button" type="button">Драма</button>
-            </li>
-            <li className="tags__list-item">
-              <button className="button tags__button" type="button">Приключение</button>
-            </li>
-            <li className="tags__list-item">
-              <button className="button tags__button" type="button">Комедия</button>
-            </li>
+            {
+              filmTags.map((tag) => <Tag key={tag.id} tag={tag} toggleTag={toggleTagId} />)
+            }
           </ul>
         </div>
       </section>
 
       <section className="cards-grid cards-grid_content_small-cards page__section">
         {
-          shownCards.map((film) => <FilmCard key={film.id} film={film} />)
+          shownCards.map((film, i) => <FilmCard key={i.toString()} film={film} />)
         }
       </section>
 
       <Pagination
-        cardsLength={films.length}
+        cardsLength={filteredFilms.length}
         onPageChange={onPageChange}
         cardsPerPage={cardsPerPage}
       />
