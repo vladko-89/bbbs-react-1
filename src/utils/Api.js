b7f7fa28c862187fe44ea70f693411270ad6ee89
script-ninja <61618647+script-ninja@users.noreply.github.com>
2021-06-20 14:30:52 +0500

    feat: авторизованный пользователь может отправить вопрос

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -129,6 +129,24 @@ class Api {
         return Promise.reject(err);
       });
   }
+
+  sendQuestion(question) {
+    const accessToken = JSON.parse(localStorage.getItem('bbbs-token')).access;
+
+    return fetch(`${this._apiUrl}/questions/`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+        authorization: `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify({ question }),
+    })
+      .then((res) => (res.json().then((data) => (res.ok ? data : Promise.reject(data)))))
+      .catch((err) => {
+        console.log(err);
+        return Promise.reject(err);
+      });
+  }
 }
 
 const api = new Api(baseUrl);
