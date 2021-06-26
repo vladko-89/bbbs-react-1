4eafad9a5e914f3f024a5705525216df151a47e6
Some-0ne-Else <cyberdef@gmail.com>
2021-06-26 15:01:21 +0300

    css module for MainQuestion

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -3,6 +3,7 @@ const baseUrl = 'http://bbbs.kiryanov.ru/api/v1';
 const delayResponse = 500;
 const scrollDelay = 100;
 const cardsPerPage = 16;
+const cardsOnMain = 4;
 const placesTextForms = ['место', 'места', 'мест'];
 const citiesList = [
   'Москва',
@@ -21,5 +22,5 @@ const citiesList = [
 ];
 
 export {
-  baseUrl, delayResponse, scrollDelay, cardsPerPage, placesTextForms, citiesList,
+  baseUrl, delayResponse, scrollDelay, cardsPerPage, cardsOnMain, placesTextForms, citiesList,
 };
