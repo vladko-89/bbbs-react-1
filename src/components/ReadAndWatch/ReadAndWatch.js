c25c98bf020a163e9e62cc5a34d854ab70e34506
Alexey <a.okovalev@yandex.ru>
2021-07-01 19:53:02 +0300

    Feature:films part was implemented for ReadAndWatch page.

--- src/components/ReadAndWatch/ReadAndWatch.js
+++ src/components/ReadAndWatch/ReadAndWatch.js
@@ -8,6 +8,12 @@ import CatalogCard, { shapes } from '../Catalog/CatalogCard/CatalogCard';
 import MainVideoPreview from '../MainVideoPreview/MainVideoPreview';
 import './ReadAndWatch.scss';
 import api from '../../utils/Api';
+import ArticleCard from '../Articles/ArticleCard/ArticleCard';
+import { mainArticle as leadArticle, articleCards } from '../../utils/articlesData';
+import { films } from '../../utils/filmsData';
+import FilmCard from '../Films/FilmCard/FilmCard';
+import BookCard from '../Books/BookCard/BookCard';
+import { books } from '../../utils/booksData';
 
 export default function ReadAndWatch(activeRubrics) {
   const [moviesToShow, setMoviesToShow] = React.useState([]);
@@ -130,10 +136,143 @@ export default function ReadAndWatch(activeRubrics) {
           </Swiper>
         </div>
       </section>
+      <section className="preview page__section">
+        <div className="preview__title-wrap">
+          <Link
+            to="./articles"
+            className="link"
+          >
+            <h3 className="chapter-title chapter-title_clickable">Статьи</h3>
+          </Link
+          >
+          <div className="preview__bottons">
+            <button className="preview__button preview__button_left articles__button_left" type="button" aria-label="buttonLeft" disabled />
+            <button className="preview__button preview__button_right articles__button_right" type="button" aria-label="buttonRight" />
+          </div>
+        </div>
+        <div className="preview__row">
+          <Swiper
+            className="preview__row preview__card catalog-card"
+            // centeredSlides="true"
+            slidesPerView="auto"
+            breakpoints={{
+              320: {
+                spaceBetween: 15,
+              },
+              720: {
+                spaceBetween: 25,
+              },
+              1024: {
+                spaceBetween: 30,
+              },
+            }}
+            navigation={{
+              prevEl: '.articles__button_left',
+              nextEl: '.articles__button_right',
+              disabledClass: 'swiper__button_disabled',
+            }}
+          >
+            {
+            // eslint-disable-next-line max-len
+            articleCards.map((card, i) => <SwiperSlide><ArticleCard key={i.toString()} {...card} /></SwiperSlide>)
+            }
+          </Swiper>
+        </div>
+      </section>
+      <section className="preview page__section">
+        <div className="preview__title-wrap">
+          <Link
+            to="./films"
+            className="link"
+          >
+            <h3 className="chapter-title chapter-title_clickable">Фильмы</h3>
+          </Link
+          >
+          <div className="preview__bottons">
+            <button className="preview__button preview__button_left films__button_left" type="button" aria-label="buttonLeft" />
+            <button className="preview__button preview__button_right films__button_right" type="button" aria-label="buttonRight" />
+          </div>
+        </div>
+        <div className="preview__row">
+          <Swiper
+            className="preview__row preview__card catalog-card"
+            // centeredSlides="true"
+            slidesPerView="auto"
+            breakpoints={{
+              320: {
+                spaceBetween: 15,
+              },
+              720: {
+                spaceBetween: 25,
+              },
+              1024: {
+                spaceBetween: 30,
+              },
+            }}
+            navigation={{
+              prevEl: '.films__button_left',
+              nextEl: '.films__button_right',
+              disabledClass: 'swiper__button_disabled',
+            }}
+          >
+            {// eslint-disable-next-line max-len
+              films.map((film, i) => (
+                <SwiperSlide>
+                  <FilmCard key={i.toString()} film={film} />
+                  {' '}
+                </SwiperSlide>
+              ))
+            }
+          </Swiper>
+        </div>
+      </section>
+      <section className="preview page__section">
+        <div className="preview__title-wrap">
+          <Link
+            to="./books"
+            className="link"
+          >
+            <h3 className="chapter-title chapter-title_clickable">Книги</h3>
+          </Link
+          >
+          <div className="preview__bottons">
+            <button className="preview__button preview__button_left books__button_left" type="button" aria-label="buttonLeft" disabled />
+            <button className="preview__button preview__button_right books__button_right" type="button" aria-label="buttonRight" />
+          </div>
+        </div>
+        <div className="preview__row ">
+          <Swiper
+            className="preview__row preview__card catalog-card"
+            // centeredSlides="true"
+            slidesPerView="auto"
+            breakpoints={{
+              320: {
+                spaceBetween: 15,
+              },
+              720: {
+                spaceBetween: 25,
+              },
+              1024: {
+                spaceBetween: 30,
+              },
+            }}
+            navigation={{
+              prevEl: '.books__button_left',
+              nextEl: '.books__button_right',
+              disabledClass: 'swiper__button_disabled',
+            }}
+          >
+            {
+              books.map((book) => <SwiperSlide><BookCard key={book.id} book={book} /></SwiperSlide>)
+            }
+          </Swiper>
+        </div>
+      </section>
     </main>
   );
 }
 
 ReadAndWatch.defaultProps = {
   activeRubrics: PropTypes.arrayOf(PropTypes.string).isRequired,
+  mainArticle: leadArticle,
 };
