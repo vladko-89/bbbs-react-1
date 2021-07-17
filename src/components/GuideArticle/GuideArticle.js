526a828d2584025413e6dc6c80214da70b8c2782
Some-0ne-Else <cyberdef@gmail.com>
2021-07-17 18:19:40 +0300

    BUG-032 fix

--- src/components/GuideArticle/GuideArticle.js
+++ src/components/GuideArticle/GuideArticle.js
@@ -1,35 +1,41 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import { useParams } from 'react-router-dom';
+import api from '../../utils/Api';
+import Preloader from '../Preloader/Preloader';
 import './GuideArticle.scss';
 
-export default function GuideArticle({ card }) {
-  return (
+export default function GuideArticle() {
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [article, setArticle] = React.useState({});
+  const { id } = useParams();
+
+  React.useEffect(() => {
+    api.getGuideArticle(id)
+      .then((res) => setArticle(res))
+      .finally(() => setIsLoading(false));
+  });
+  return isLoading ? <Preloader /> : (
     <main className="main">
       <section className="article page__section">
-        <h1 className="chapter-title article__main-title">{card.title}</h1>
+        <h1 className="chapter-title article__main-title">{article.title}</h1>
         <p className="section-title article__description">
-          {card.description}
+          {article.description}
         </p>
         <figure className="article__figure">
           <img
-            src={card.imageUrl}
-            alt={card.title}
+            src={article.imageUrl}
+            alt={article.title}
             className="article__image"
           />
           <figcaption className="caption article__figcaption">
-            {card.imageCaption}
+            {article.imageCaption}
           </figcaption>
         </figure>
 
         { /* eslint-disable-next-line react/no-danger */}
-        <div className="article_" dangerouslySetInnerHTML={{ __html: card.text }} />
+        <div className="article_" dangerouslySetInnerHTML={{ __html: article.text }} />
 
       </section>
     </main>
   );
 }
-GuideArticle.propTypes = {
-  card: PropTypes.shape(PropTypes.shape({
-    id: PropTypes.string.isRequired,
-  })).isRequired,
-};
